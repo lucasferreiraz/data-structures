eeecc7f4640f1dccf5378d91ff963c493e9e06ba
Lucas Ferreira <ferreiralucas090@gmail.com>
2022-08-29 15:33:05 -0300

    feat(Vector): add, getAt, search, increaseCapacity

--- data-structures/src/App.java
+++ data-structures/src/App.java
@@ -3,11 +3,17 @@ import structures.Vector;
 public class App {
     public static void main(String[] args) throws Exception {
         
-        Vector vector = new Vector(2);
+        Vector vector = new Vector(6);
 
-        vector.add(10);
-        vector.add(20);
-        vector.add(30);
+        vector.add( 10); //0
+        vector.add(20); //1
+        vector.add(30); //2
+        vector.add(40); //3
+        vector.add(50); //4
+        vector.add(60); //5
+
+        vector.add(70);
+        vector.add(80);
 
     }
 }
--- data-structures/src/structures/Vector.java
+++ data-structures/src/structures/Vector.java
@@ -1,28 +1,70 @@
 package structures;
 
 public class Vector {
-    
+
     private Integer vector[];
     private int size;
 
-    public Vector(int amount){
+    public Vector(int amount) {
         this.vector = new Integer[amount];
         this.size = 0;
     }
 
-    //add element at the end of the vector
-    public void add(int number){
-        if(size < vector.length){
-            vector[size] = number;
+    public void add(int element) {
+        increaseCapacity();
+        if (size < vector.length) {
+            vector[size] = element;
             size++;
         } else {
             throw new RuntimeException("error adding element out of vector bounds");
         }
     }
 
-    //return the real size of vector
+    public void add(int position, int element){
+        if (!(position < size && position >= 0)) {
+            throw new IllegalArgumentException("element out of vector range");
+        }
+
+        increaseCapacity();
+
+        //0 1 2 3 4 5 6 7 
+        //B C D E F G * *
+        //B C D E E F G * (move)
+        //B C D K E F G * (assignment)
+
+        for(int i = size; i > position; i--){
+            vector[i] = vector[i-1];
+        }
+        vector[position] = element;
+    }
+
+    public Integer getAt(int position) {
+        if (position < size && position >= 0) {
+            return vector[position];
+        }
+        throw new IllegalArgumentException("element out of vector range");
+    }
+
+    public int search(Integer element){
+        for(int i = 0; i < size; i++){
+            if(vector[i].equals(element)) { return i; }
+        }
+        
+        return -1;
+    }
+
     public int getSize() {
         return size;
+    }
+
+    public void increaseCapacity(){
+        if(size == vector.length){
+            Integer newVector[] = new Integer[size + 1];
+            for(int i = 0; i < vector.length; i++){
+                newVector[i] = vector[i];
+            }
+            vector = newVector;
+        }
     }
 
 }

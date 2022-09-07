388aea8632f45c5100cadeeb1b30350e05499c6f
Lucas Ferreira <ferreiralucas090@gmail.com>
2022-09-07 11:57:17 -0300

    feat(Stack): push, getSize, increaseCapacity

---
+++ data-structures/src/structures/Stack.java
@@ -0,0 +1,56 @@
+package structures;
+
+@SuppressWarnings("unchecked")
+public class Stack<T> {
+    
+    private T vector[];
+    private int size;
+
+    public Stack(int amount) {
+        this.vector = (T[]) new Object[amount];
+        this.size = 0;
+    }
+
+    public void push(T element) {
+        increaseCapacity();
+        if (size < vector.length) {
+            vector[size] = element;
+            size++;
+        } else {
+            throw new RuntimeException("error adding element out of vector bounds");
+        }
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void increaseCapacity(){
+        if(size == vector.length){
+            T newVector[] = (T[]) new Object[size + 1];
+            for(int i = 0; i < vector.length; i++){
+                newVector[i] = vector[i];
+            }
+            vector = newVector;
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+
+        for(int i = 0; i < size-1; i++){
+            sb.append(vector[i]);
+            sb.append(", ");
+        }
+
+        if(size > 0){
+            sb.append(vector[size-1]);
+            sb.append("]");
+        }
+
+        return sb.toString();
+    }
+
+}
